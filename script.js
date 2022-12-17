3c0f795e74da6b797a7d4117c08a5df17f4e2484
Amr Bedir <amr.bedir.1@gmail.com>
2022-12-17 15:52:37 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+          <script type='text/javascript'>
+            // Check that service workers are supported
+            if ('serviceWorker' in navigator) {
+                // Use the window load event to keep the page load performant
+                window.addEventListener('load', () => {
+                    navigator.serviceWorker.register('/service-worker.js');
+                });
+            }
+           </script>
